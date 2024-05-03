eb4a19939b83eedf4922f9a39e4f4deff023a493
vhtuananh020402 <vhtuananh020402@gmail.com>
2024-05-04 00:01:24 +0700

    e28.cpp

--- e28.cpp
+++ e28.cpp
@@ -1,20 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
 
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        int neddleIndx = 0;
+        int needleIndx = 0;
         int result = -1;
         for (int i = 0; i < haystack.length(); ++i) {
             if (haystack[i] == needle[needleIndx]) {
-                if (neddleIndx == 0) {
+                if (needleIndx == 0) {
                     result = i;
                 }
-                neddleIndx++;
-                if (neddleIndx == needle.length() - 1) {
+                needleIndx++;
+                if (needleIndx == needle.length() - 1) {
                     return result;
                 }
             } else {
-                neddleIndx = 0;
+                needleIndx = 0;
             }
         }
         return -1;
