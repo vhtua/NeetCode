1f079633193d2e02391502ff95b399a9de310aac
Vu Hoang Tuan Anh <73384816+vhtuananh020402@users.noreply.github.com>
2024-05-06 09:00:46 +0000

    arrayChallenge

--- arrayChallange.cpp
+++ arrayChallange.cpp
@@ -5,19 +5,30 @@
 using namespace std;
 
 vector<string> arrayChallange(vector<string> &strArr) {
-    
-    return 
+    vector<string> result;
+
+    for (int i = 0; i < (int)strArr.size(); ++i) {
+        
+    }
+
+    return strArr;
 }
 
 void printArr(vector<string> &strArr) {
     for (int i = 0; i < (int) strArr.size(); ++i) {
         cout << strArr[i] << " -- ";
     }
+    cout << endl;
 }
 
 int main() {
     vector<string> strArr = {"A", "B", "A", "C", "A", "B"};
+    vector<string> strArr2 = {"A", "B", "C", "D", "E", "D", "Q", "Z", "C"};
     vector<string> result = arrayChallange(strArr);
+    vector<string> result2 = arrayChallange(strArr2);
+    cout << "-- Test case #1 ---------------------------\n";
     printArr(result);
+    cout << "-- Test case #2 ---------------------------\n";
+    printArr(result2);
     return 0;
 }
