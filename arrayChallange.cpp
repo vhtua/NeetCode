720a0894b0437d48c2dfa755a7f0bfb811cbf68e
Vu Hoang Tuan Anh <73384816+vhtuananh020402@users.noreply.github.com>
2024-05-06 08:47:05 +0000

    update/printArr

--- arrayChallange.cpp
+++ arrayChallange.cpp
@@ -9,9 +9,15 @@ vector<string> arrayChallange(vector<string> &strArr) {
     return 
 }
 
+void printArr(vector<string> &strArr) {
+    for (int i = 0; i < (int) strArr.size(); ++i) {
+        cout << strArr[i] << " -- ";
+    }
+}
 
 int main() {
     vector<string> strArr = {"A", "B", "A", "C", "A", "B"};
-    arra
+    vector<string> result = arrayChallange(strArr);
+    printArr(result);
     return 0;
 }
